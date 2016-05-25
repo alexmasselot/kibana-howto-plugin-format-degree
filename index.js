0543e32ae10ebc90d2e9ee3d9aedba71974a6f34
Alexandre Masselot <alexandre.masselot@gmail.com>
2016-05-25 08:18:18 +0200

    packaging for kibana

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 module.exports = function (kibana) {
   return new kibana.Plugin({
 
-    name: 'simple_format_degree',
+    name: 'simple_format',
     require: ['kibana', 'elasticsearch'],
     uiExports: {
       // fieldFormat is not working as of Kibana 4.3.0, but it's fixed in the master branch
       // see the issue on GitHub: https://github.com/elastic/kibana/issues/6040
       // As a workaround, just add the field formats to visTypes array.
-      fieldFormats: ['plugins/simple_format/simple_format_degree'],
-      visTypes: [
-        'plugins/simple_format/simple_format_degree'
-      ]
+      fieldFormats: ['plugins/simple_format/degree']
     }
   });
 };
